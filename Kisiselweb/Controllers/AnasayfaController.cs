ed97daf91b24255987e01fb28158ea9a0d0cd189
YesimEren <sertyessim@gmail.com>
2023-12-15 17:32:22 +0300

    Add project files.

---
+++ Kisiselweb/Controllers/AnasayfaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Kisiselweb.Controllers
+{
+    public class AnasayfaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
