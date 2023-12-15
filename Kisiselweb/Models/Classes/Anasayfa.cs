7bf67e33b053527d35fb1d5eca51577d76652a8d
YesimEren <sertyessim@gmail.com>
2023-12-15 18:20:06 +0300

    reposi

---
+++ Kisiselweb/Models/Classes/Anasayfa.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.CodeAnalysis.Options;
+using System.ComponentModel.DataAnnotations;
+
+namespace Kisiselweb.Models.Classes
+{
+    public class Anasayfa
+    {
+        [Key]
+        public int id { get; set; }
+        public string Profiphoto { get; set; }
+        public string Description { get; set; }
+        public string Unvan { get; set; }
+        public string SocialMedia { get; set; }
+    }
+}
---
+++ Kisiselweb/Models/Classes/Icons.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace Kisiselweb.Models.Classes
+{
+    public class Icons
+    {
+        public int İd { get; set; }
+        public string Icon { get; set; }
+        public string Linj { get; set; }
+    }
+}
